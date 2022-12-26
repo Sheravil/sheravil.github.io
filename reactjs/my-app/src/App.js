370fec89afd90cd3e268f83e917c50bdaf506c68
Sheravil <Sheravil>
2022-12-26 14:59:59 +0300

    furki

---
+++ reactjs/my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ResponsiveAppBar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+    <ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default App;
