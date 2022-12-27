6f602a72198610724350711c85782d5137d15bcc
Sheravil <Sheravil>
2022-12-27 15:33:02 +0300

    updated about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,20 +5,45 @@ import Grid from "@mui/material/Grid";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import Avatar from "@mui/material/Avatar";
-
+import { color } from "@mui/system";
+import { BorderColor } from "@mui/icons-material";
 export default function AboutMe() {
   return (
-    <Box height="95vh" bgcolor="rgba(50, 100, 83, 0.3)">
-      <Grid container height="100%" alignItems="center" padding={30}>
+    <Box height="95vh" bgcolor="rgba(50, 100, 83, 0.4)" fontFamily={"Arial"}>
+      <Grid
+        container
+        height="100%"
+        alignItems="center"
+        padding={30}
+        color={"darkcyan"}
+        // color={"rgba(234, 253, 252,0.8)"}
+      >
         <Grid container item xs={8} justifyContent="center" alignItems="center">
-          <Grid item xs={12} alignSelf="flex-start" textAlign="left">
+          <Grid
+            item
+            xs={12}
+            alignSelf="flex-start"
+            textAlign="left"
+            fontSize={30}
+          >
             <p>Hi there 👋, I'm</p>
-            <br />
-            <h1 className="hh1">Emin Çuha</h1>
           </Grid>
-          <Grid item xs={6}>
+          <Grid
+            item
+            xs={12}
+            alignSelf="flex-start"
+            textAlign="left"
+            fontSize={60}
+            margin={-15}
+            marginLeft={-17}
+          >
+            <h1>Emin Çuha</h1>
+          </Grid>
+          <Grid item xs={6} fontSize={30}>
             <p>Front-End Developer</p>
-            <p className="emailFont">emincuha@gmail.com</p>
+            <p style={{ fontSize: "20px", marginTop: "-30px" }}>
+              emincuha@gmail.com
+            </p>
           </Grid>
           <Grid item xs={6} textAlign="left">
             <LinkedInIcon fontSize="large" />
@@ -29,7 +54,11 @@ export default function AboutMe() {
           <Avatar
             alt="Remy Sharp"
             src="https://media.licdn.com/dms/image/D4D03AQG9mkoN9LNp2w/profile-displayphoto-shrink_800_800/0/1670255372337?e=1677715200&v=beta&t=SHia7ha4Rd1tbEWMpzZY-wg2EnnCmv7AeiExpDvPsc8"
-            sx={{ width: 350, height: 350, float: "right" }}
+            sx={{
+              width: 350,
+              height: 350,
+              float: "right",
+            }}
           />
         </Grid>
       </Grid>
--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,1 +1,1 @@
-export { default } from "./AboutMe";
+export { default } from "./AboutMe.js";
